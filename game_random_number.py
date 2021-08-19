3253e6174743af12f298899410704743372fc59b
Екатерина Костицына <kostitsyna.e@gmail.com>
2021-08-19 16:46:11 +0300

    Добавила функцию new_game

--- game_random_number.py
+++ game_random_number.py
@@ -1,9 +1,6 @@
 import random
 from math import ceil
 
-print('Добро пожаловать в числовую угадайку')
-print('Я загадал число от 1 до 100, сможешь угадать какое?')
-
 def is_valid(number):
     # Проверка на то, что пользователь действительно введет число и в нужном диапазоне
     if number.isdigit() and 1 <= int(number) <= 101:
@@ -16,7 +13,9 @@ def the_game():
     random_number = random.randint(1, 100)
     # Число попыток пользователя
     tries_of_user = 0
-    while True:
+    flag = True
+
+    while flag:
         user_guess = input('Введи свое число: ')
         if is_valid(user_guess):
             user_guess = ceil(int(user_guess))
@@ -28,6 +27,23 @@ def the_game():
                 print('Твое число больше того, что я загадал. Попробуй еще раз..')
             if user_guess == random_number:
                 print('Ух ты! Ты угадал мое число!', f'Попыток понадобилось -> {tries_of_user}')
+                new_game()
+                flag = False
+
+# Новая игра
+def new_game():
+    question = input('Хочешь сыграть еще раз? Напиши да или нет -> ')
+    flag = True
+    while flag:
+        if question.lower() == 'да':
+            the_game()
+        elif question.lower() == 'нет':
+            return print('Еще увидимся!')
+            flag = False
+        else:
+            question = input("Хитрюга, но меня не проведешь! Все же введи 'да' или 'нет' -> ")
+           
+print('Добро пожаловать в числовую угадайку')
+print('Я загадал число от 1 до 100, сможешь угадать какое?')
 
 the_game()
-print('Еще увидимся!')
